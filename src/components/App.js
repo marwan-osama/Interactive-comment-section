f2d8f695158212e6de1d7e6176c5db4401adba9b
marwan osama <mo989989@gmail.com>
2022-08-06 14:38:09 +0200

    feat: use localStorage to store comments data

--- src/components/App.js
+++ src/components/App.js
@@ -2,78 +2,80 @@ import { useEffect, useState } from "react";
 import Comment from "./Comment";
 import NewComment from "./NewComment";
 
-const data = {
-	currentUser: {
-		image: {
-			png: "/images/avatars/image-juliusomo.png",
-			webp: "/images/avatars/image-juliusomo.webp",
-		},
-		username: "juliusomo",
-	},
-	comments: [
-		{
-			id: 1,
-			content:
-				"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-			createdAt: "1 month ago",
-			score: 12,
-			user: {
+const data = localStorage.getItem("commentSectionData")
+	? JSON.parse(localStorage.getItem("commentSectionData"))
+	: {
+			currentUser: {
 				image: {
-					png: "/images/avatars/image-amyrobson.png",
-					webp: "/images/avatars/image-amyrobson.webp",
+					png: "/images/avatars/image-juliusomo.png",
+					webp: "/images/avatars/image-juliusomo.webp",
 				},
-				username: "amyrobson",
+				username: "juliusomo",
 			},
-			replies: [],
-		},
-		{
-			id: 2,
-			content:
-				"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-			createdAt: "2 weeks ago",
-			score: 5,
-			user: {
-				image: {
-					png: "/images/avatars/image-maxblagun.png",
-					webp: "/images/avatars/image-maxblagun.webp",
-				},
-				username: "maxblagun",
-			},
-			replies: [
+			comments: [
 				{
-					id: 3,
+					id: 1,
 					content:
-						"If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-					createdAt: "1 week ago",
-					score: 4,
-					replyingTo: "maxblagun",
+						"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+					createdAt: "1 month ago",
+					score: 12,
 					user: {
 						image: {
-							png: "/images/avatars/image-ramsesmiron.png",
-							webp: "/images/avatars/image-ramsesmiron.webp",
+							png: "/images/avatars/image-amyrobson.png",
+							webp: "/images/avatars/image-amyrobson.webp",
 						},
-						username: "ramsesmiron",
+						username: "amyrobson",
 					},
+					replies: [],
 				},
 				{
-					id: 4,
+					id: 2,
 					content:
-						"I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-					createdAt: "2 days ago",
-					score: 2,
-					replyingTo: "ramsesmiron",
+						"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+					createdAt: "2 weeks ago",
+					score: 5,
 					user: {
 						image: {
-							png: "/images/avatars/image-juliusomo.png",
-							webp: "/images/avatars/image-juliusomo.webp",
+							png: "/images/avatars/image-maxblagun.png",
+							webp: "/images/avatars/image-maxblagun.webp",
 						},
-						username: "juliusomo",
+						username: "maxblagun",
 					},
+					replies: [
+						{
+							id: 3,
+							content:
+								"If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+							createdAt: "1 week ago",
+							score: 4,
+							replyingTo: "maxblagun",
+							user: {
+								image: {
+									png: "/images/avatars/image-ramsesmiron.png",
+									webp: "/images/avatars/image-ramsesmiron.webp",
+								},
+								username: "ramsesmiron",
+							},
+						},
+						{
+							id: 4,
+							content:
+								"I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+							createdAt: "2 days ago",
+							score: 2,
+							replyingTo: "ramsesmiron",
+							user: {
+								image: {
+									png: "/images/avatars/image-juliusomo.png",
+									webp: "/images/avatars/image-juliusomo.webp",
+								},
+								username: "juliusomo",
+							},
+						},
+					],
 				},
 			],
-		},
-	],
-};
+	  };
 
 function App() {
 	const [comments, setComments] = useState(data.comments);
@@ -140,10 +142,13 @@ function App() {
 	};
 
 	useEffect(() => {
-		localStorage.setItem("data", {
-			currentUser: data.currentUser,
-			comments: comments,
-		});
+		localStorage.setItem(
+			"commentSectionData",
+			JSON.stringify({
+				currentUser: data.currentUser,
+				comments,
+			})
+		);
 	}, [comments]);
 
 	return (
